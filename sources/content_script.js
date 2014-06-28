eb9b10d1a192074678a23486ff317adb09056981
Richard Harrington <harrington.richard@gmail.com>
2014-06-28 07:45:37 -0400

    refactored substitutions module

--- sources/content_script.js
+++ sources/content_script.js
@@ -1,66 +1,53 @@
 var DISRUPTION_SUBSTITUTIONS = (function() {
 
-  // "<helping verb> <optional adverb> disrupted" =>
-  // "<helping verb> <optional adverb> drowned in bullshit"
-  var specialSubstitutions = [
-    [ /(be|being|been|is|are|were|get|gets)\s+(\w+\s+)?disrupted/g, '$1 $2 covered in bullshit'],
-    [ /(Be|Being|Been|Is|Are|Were|Get|Gets)\s+(\w+\s+)?Disrupted/g, '$1 $2 Covered in Bullshit']
+  var endingMap = {
+    ed: 'rained bullshit on',
+    ively: 'by means of bullshit',
+    ive: 'bullshit',
+    ions: 'piles of bullshit',
+    ion: 'bullshit',
+    ing: 'raining bullshit on',
+    or: 'bullshitter',
+    s: 'rains bullshit on',
+    '': 'rain bullshit on'
+  };
+
+  var substitutions = [
+
+    // "<helping verb> <optional adverb> disrupted" =>
+    // "<helping verb> <optional adverb> covered in bullshit"
+    [ /(be|being|been|is|are|were|get|gets)\s+(\w+\s+)?disrupted/g, '$1 $2 covered in bullshit' ],
+    [ /(Be|Being|Been|Is|Are|Were|Get|Gets)\s+(\w+\s+)?Disrupted/g, '$1 $2 Covered in Bullshit' ],
+
+    [ /so disruptive/g, 'such bullshit' ],
+    [ /So Disruptive/g, 'Such Bullshit' ],
+    [ /so disruptively/g, 'by means of such bullshit' ],
+    [ /So Disruptively/g, 'By Means Of Such Bullshit' ],
+    [ /-disrupting/g, '-bullshitting' ],
+    [ /-Disrupting/g, '-Bullshitting' ],
+    [ /\b([Dd])isrupt(ed|ively|ive|ions|ion|ing|or|s)?\b/g, function(_, firstLetter, ending) {
+        var isUpperCase = firstLetter === 'D';
+        var phrase = endingMap[ending || ''];
+        return isUpperCase ? capitalizePhrase(phrase) : phrase;
+    }]
   ];
 
-  var lowerCasePhrasePairs = [
-    [ 'so disruptive', 'such bullshit' ],
-    [ 'disrupted', 'rained bullshit on' ],
-    [ 'disruptive', 'bullshit' ],
-    [ 'disruptions', 'piles of bullshit' ],
-    [ 'disruption', 'bullshit' ],
-    [ '-disrupting', '-bullshitting' ],
-    [ 'disrupting', 'raining bullshit on' ],
-    [ 'disruptor', 'bullshitter' ],
-    [ 'disrupts', 'rains bullshit on' ],
-    [ 'disrupt', 'rain bullshit on' ],
-  ];
-
-  var substitutions = generateSubstitutions();
-
-  // In the generateSubstitutions function, the information will be compiled in the order listed above.
-
-  function generateSubstitutions() {
-    var upperCasePhrasePairs = lowerCasePhrasePairs.map(capitalizePhrases);
-    var phrasePairs = [].concat(lowerCasePhrasePairs, upperCasePhrasePairs);
-    var regularSubstitutions = phrasePairs.map(generateSubstitution);
-
-    return [].concat(specialSubstitutions, regularSubstitutions);
-  }
-
-  function capitalizePhrases(phrases) {
-    return phrases.map(function(phrase) {
-      return phrase.split(" ").map(capitalizeWord).join(" ");
-    });
+  function capitalizePhrase(phrase) {
+    return phrase.split(' ').map(function(word) {
+      return (word.length > 2) ? capitalizeWord(word) : word;
+    }).join(' ');
   }
 
   function capitalizeWord(word) {
-    // A word may start with a hyphen
-    var firstLetterIdx = (word[0] === '-') ? 1 : 0;
-    var firstLetter = word[firstLetterIdx];
-    return word.substr(0, firstLetterIdx) + capitalizeLetter(firstLetter) + word.substr(firstLetterIdx + 1);
-  }
-
-  function capitalizeLetter(letter) {
-    return String.fromCharCode(letter.charCodeAt(0) - 32);
-  }
-
-  function generateSubstitution(pair) {
-    var regExp = new RegExp(pair[0], "g");
-    var replacement = pair[1];
-    return [regExp, replacement];
+    var firstLetterCode = word.charCodeAt(0);
+    var capitalizedFirstLetter = String.fromCharCode(firstLetterCode - 32);
+    return capitalizedFirstLetter + word.substr(1);
   }
 
   return {
     substitute: function(originalText) {
       return substitutions.reduce(function(text, substitution) {
-        var regExp = substitution[0];
-        var replacement = substitution[1];
-        return text.replace(regExp, replacement);
+        return text.replace(substitution[0], substitution[1]);
       }, originalText);
     }
   };
