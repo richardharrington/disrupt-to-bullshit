8556c072ce26715c186a47306b3e48493def02b1
Richard Harrington <harrington.richard@gmail.com>
2014-06-30 23:42:46 -0400

    an experiment in polling for scrolling

--- sources/main.js
+++ sources/main.js
@@ -1,7 +1,35 @@
 (function() {
-  var disruptToBullshit = window.createVerbConverter(window.DISRUPT_TO_BULLSHIT_RULES);
-  window.walkTextNodes(document.body, function(node) {
-    node.nodeValue = disruptToBullshit(node.nodeValue);
-  });
+  var createVerbConverter = window.createVerbConverter;
+  var DISRUPT_TO_BULLSHIT_RULES = window.DISRUPT_TO_BULLSHIT_RULES;
+  var walkTextNodes = window.walkTextNodes;
+
+  var disruptToBullshit = createVerbConverter(DISRUPT_TO_BULLSHIT_RULES);
+  var lastY = 0;
+  var hasScrolled = false;
+
+  refresh();
+  window.addEventListener('scroll', startPollingTheFirstTimeWeScroll);
+
+  function refresh() {
+    walkTextNodes(document.body, function(node) {
+      node.nodeValue = disruptToBullshit(node.nodeValue);
+    });
+  };
+
+  function startPollingTheFirstTimeWeScroll() {
+    if (hasScrolled)
+      return;
+    hasScrolled = true;
+    setInterval(refreshIfWeHaveJumpedUpOrDownAScreen, 1000);
+  }
+
+  function refreshIfWeHaveJumpedUpOrDownAScreen() {
+    if (Math.abs(window.scrollY - lastY) > window.innerHeight) {
+      lastY = window.scrollY;
+      refresh();
+    }
+  }
+
 })();
+
 
