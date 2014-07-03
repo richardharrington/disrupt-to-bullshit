f48e541eb7939e44c425b9d0e97406db0024d9bd
Richard Harrington <harrington.richard@gmail.com>
2014-07-03 00:00:59 -0400

    changed license to Mozilla

--- sources/content_script.js
+++ sources/content_script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2014 Richard Harrington.
-// Use of this source code is governed by the MIT license,
-// found in the LICENSE file.
-// (inspired by the various people who made cloud-to-butt.)
+// Use of this source code is governed by the Mozilla Public License 2.0.
+
+// (and inspired by people who made Cloud to Butt.)
 
 (function() {
 
