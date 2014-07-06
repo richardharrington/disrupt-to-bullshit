8ef9d59b2f44abfa41f9db32ec2436fd84e34945
Richard Harrington <harrington.richard@gmail.com>
2014-07-06 19:47:02 -0400

    changed conversion symbol in test descriptions from 'to' to '=>'

--- spec/create_verb_converter_spec.js
+++ spec/create_verb_converter_spec.js
@@ -16,118 +16,118 @@ describe("createVerbConverter", function() {
 
   describe("transitive verb", function() {
 
-    assert('disrupt to rain bullshit on',
+    assert('disrupt => rain bullshit on',
            'disrupt this place', 'rain bullshit on this place');
 
-    assert('disrupted to rained bullshit on',
+    assert('disrupted => rained bullshit on',
            'disrupted this place', 'rained bullshit on this place');
 
-    assert('disrupts to rains bullshit on',
+    assert('disrupts => rains bullshit on',
            'disrupts this place', 'rains bullshit on this place');
 
-    assert('disrupting to raining bullshit on',
+    assert('disrupting => raining bullshit on',
            'disrupting this place', 'raining bullshit on this place');
 
-    assert('Disrupt to Rain Bullshit on',
+    assert('Disrupt => Rain Bullshit on',
            'Disrupt this place', 'Rain Bullshit on this place');
 
-    assert('Disrupted to Rained Bullshit on',
+    assert('Disrupted => Rained Bullshit on',
            'Disrupted this place', 'Rained Bullshit on this place');
 
-    assert('Disrupts to Rains Bullshit on',
+    assert('Disrupts => Rains Bullshit on',
            'Disrupts this place', 'Rains Bullshit on this place');
 
-    assert('Disrupting to Raining Bullshit on',
+    assert('Disrupting => Raining Bullshit on',
            'Disrupting this place', 'Raining Bullshit on this place');
 
-    assert('disrupt to rain bullshit on when surrounded by single straight quotes',
+    assert('disrupt => rain bullshit on when surrounded by single straight quotes',
            '\'disrupt\' this', '\'rain bullshit on\' this');
 
-    assert('disrupt to rain bullshit on when surrounded by single curly quotes',
+    assert('disrupt => rain bullshit on when surrounded by single curly quotes',
            '‘disrupt’ this', '‘rain bullshit on’ this');
 
-    assert('disrupt to rain bullshit on when surrounded by double straight quotes',
+    assert('disrupt => rain bullshit on when surrounded by double straight quotes',
            '"disrupt" this', '"rain bullshit on" this');
 
-    assert('disrupt to rain bullshit on when surrounded by double curly quotes',
+    assert('disrupt => rain bullshit on when surrounded by double curly quotes',
            '“disrupt” this', '“rain bullshit on” this');
 
-    assert('disrupt to rain bullshit on when surrounded by single and double quotes',
+    assert('disrupt => rain bullshit on when surrounded by single and double quotes',
            '“‘disrupt’” this', '“‘rain bullshit on’” this');
 
   });
 
   describe("noun", function() {
 
-    assert('disruption to bullshit',
+    assert('disruption => bullshit',
            'disruption', 'bullshit');
 
-    assert('disruptions to piles of bullshit',
+    assert('disruptions => piles of bullshit',
            'disruptions', 'piles of bullshit');
 
-    assert('disruptor to bullshitter',
+    assert('disruptor => bullshitter',
            'disruptor', 'bullshitter');
 
-    assert('disrupter to bullshitter',
+    assert('disrupter => bullshitter',
            'disrupter', 'bullshitter');
 
-    assert('disruptors to bullshitters',
+    assert('disruptors => bullshitters',
            'disruptors', 'bullshitters');
 
-    assert('disrupters to bullshitters',
+    assert('disrupters => bullshitters',
            'disrupters', 'bullshitters');
 
-    assert('Disruption to Bullshit',
+    assert('Disruption => Bullshit',
            'Disruption', 'Bullshit');
 
-    assert('Disruptions to Piles of Bullshit',
+    assert('Disruptions => Piles of Bullshit',
            'Disruptions', 'Piles of Bullshit');
 
-    assert('Disruptor to Bullshitter',
+    assert('Disruptor => Bullshitter',
            'Disruptor', 'Bullshitter');
 
-    assert('Disrupter to Bullshitter',
+    assert('Disrupter => Bullshitter',
            'Disrupter', 'Bullshitter');
 
-    assert('Disruptors to Bullshitters',
+    assert('Disruptors => Bullshitters',
            'Disruptors', 'Bullshitters');
 
-    assert('Disrupters to Bullshitters',
+    assert('Disrupters => Bullshitters',
            'Disrupters', 'Bullshitters');
 
   });
 
   describe("adjective and adverb", function() {
 
-    assert('disruptive to bullshit',
+    assert('disruptive => bullshit',
            'disruptive', 'bullshit');
 
-    assert('Disruptive to Bullshit',
+    assert('Disruptive => Bullshit',
            'Disruptive', 'Bullshit');
 
-    assert('disruptively to bullshittingly',
+    assert('disruptively => bullshittingly',
            'disruptively', 'bullshittingly');
 
-    assert('Disruptively to Bullshittingly',
+    assert('Disruptively => Bullshittingly',
            'Disruptively', 'Bullshittingly');
 
   });
 
   describe("past participle in passive construction", function() {
 
-    assert('<LC helping verb> disrupted to <LC helping verb>covered in bullshit',
+    assert('<LC helping verb> disrupted => <LC helping verb>covered in bullshit',
            'got disrupted', 'got covered in bullshit');
 
-    assert('<LC helping verb> <LC adverb> totally disrupted to <LC helping verb> <LC adverb> totally covered in bullshit',
+    assert('<LC helping verb> <LC adverb> totally disrupted => <LC helping verb> <LC adverb> totally covered in bullshit',
            'got totally disrupted', 'got totally covered in bullshit');
 
-    assert('<UC helping verb> Disrupted to <UC helping verb> Covered in Bullshit',
+    assert('<UC helping verb> Disrupted => <UC helping verb> Covered in Bullshit',
            'Got Disrupted', 'Got Covered in Bullshit');
 
-    assert('<LC helping verb> Disrupted to <LC helping verb> Covered in Bullshit',
+    assert('<LC helping verb> Disrupted => <LC helping verb> Covered in Bullshit',
            'got Disrupted', 'got Covered in Bullshit');
 
-    assert('<LC helping verb> <UC adverb> Disrupted to <LC helping verb> <UC adverb> Covered in Bullshit',
+    assert('<LC helping verb> <UC adverb> Disrupted => <LC helping verb> <UC adverb> Covered in Bullshit',
            'got Totally Disrupted', 'got Totally Covered in Bullshit');
 
     assert('tightens up extra spaces',
@@ -137,110 +137,110 @@ describe("createVerbConverter", function() {
 
   describe("looks like transitive verb but is probably intransitive verb or noun", function() {
 
-    assert('disrupt to bullshit at end of string',
+    assert('disrupt => bullshit at end of string',
            'disrupt', 'bullshit');
 
-    assert('disrupting to bullshitting when at end of string',
+    assert('disrupting => bullshitting when at end of string',
            'disrupting', 'bullshitting');
 
-    assert('disrupted to bullshitted when at end of string',
+    assert('disrupted => bullshitted when at end of string',
            'disrupting', 'bullshitting');
 
-    assert('disrupts to bullshits when at end of string',
+    assert('disrupts => bullshits when at end of string',
            'disrupting', 'bullshitting');
 
-    assert('Disrupt to Bullshit at end of string',
+    assert('Disrupt => Bullshit at end of string',
            'Disrupt', 'Bullshit');
 
-    assert('Disrupting to Bullshitting when at end of string',
+    assert('Disrupting => Bullshitting when at end of string',
            'Disrupting', 'Bullshitting');
 
-    assert('Disrupted to Bullshitted when at end of string',
+    assert('Disrupted => Bullshitted when at end of string',
            'Disrupting', 'Bullshitting');
 
-    assert('Disrupts to Bullshits when at end of string',
+    assert('Disrupts => Bullshits when at end of string',
            'Disrupting', 'Bullshitting');
 
-    assert('disrupt to bullshit when at end of string surrounded by single straight quotes',
+    assert('disrupt => bullshit when at end of string surrounded by single straight quotes',
            '\'disrupt\'', '\'bullshit\'');
 
-    assert('disrupt to bullshit when at end of string surrounded by double straight quotes',
+    assert('disrupt => bullshit when at end of string surrounded by double straight quotes',
            '"disrupt"', '"bullshit"');
 
-    assert('disrupt to bullshit when at end of string surrounded by single curly quotes',
+    assert('disrupt => bullshit when at end of string surrounded by single curly quotes',
            '‘disrupt’', '‘bullshit’');
 
-    assert('disrupt to bullshit when at end of string surrounded by double curly quotes',
+    assert('disrupt => bullshit when at end of string surrounded by double curly quotes',
            '“disrupt”', '“bullshit”');
 
-    assert('disrupt to bullshit when at end of string surrounded by single and double quotes',
+    assert('disrupt => bullshit when at end of string surrounded by single and double quotes',
            '“‘disrupt’”', '“‘bullshit’”');
 
-    assert('disrupt to bullshit when followed by comma',
+    assert('disrupt => bullshit when followed by comma',
            'disrupt,', 'bullshit,');
 
-    assert('disrupt to bullshit when followed by dash',
+    assert('disrupt => bullshit when followed by dash',
            'disrupt-', 'bullshit-');
 
-    assert('disrupt to bullshit when followed by en-dash',
+    assert('disrupt => bullshit when followed by en-dash',
            'disrupt–', 'bullshit–');
 
-    assert('disrupt to bullshit when followed by em-dash',
+    assert('disrupt => bullshit when followed by em-dash',
            'disrupt—', 'bullshit—');
 
-    assert('disrupt to bullshit when followed by colon',
+    assert('disrupt => bullshit when followed by colon',
            'disrupt:', 'bullshit:');
 
-    assert('disrupt to bullshit when followed by semicolon',
+    assert('disrupt => bullshit when followed by semicolon',
            'disrupt;', 'bullshit;');
 
-    assert('disrupt to bullshit when followed by period',
+    assert('disrupt => bullshit when followed by period',
            'disrupt.', 'bullshit.');
 
-    assert('disrupt to bullshit when followed by question mark',
+    assert('disrupt => bullshit when followed by question mark',
            'disrupt?', 'bullshit?');
 
-    assert('disrupt to bullshit when followed by exclamation point',
+    assert('disrupt => bullshit when followed by exclamation point',
            'disrupt!', 'bullshit!');
 
     assert('deals with optional multiple spaces before punctuation',
            'disrupt     :', 'bullshit     :');
 
-    assert('disrupt to bullshit when followed by single straight quote and preceded by something other than single straight quote',
+    assert('disrupt => bullshit when followed by single straight quote and preceded by something other than single straight quote',
            'It is not disrupt\'s fault', 'It is not bullshit\'s fault');
 
-    assert('disrupt to bullshit when followed by single straight quote and preceded by something other than single straight quote',
+    assert('disrupt => bullshit when followed by single straight quote and preceded by something other than single straight quote',
            'It is not disrupt’s fault', 'It is not bullshit’s fault');
 
-    assert('disrupt to bullshit when starting the string and followed by single straight quote',
+    assert('disrupt => bullshit when starting the string and followed by single straight quote',
            'disrupt\'', 'bullshit\'');
 
-    assert('disrupt to bullshit when starting the string and followed by single curly quote',
+    assert('disrupt => bullshit when starting the string and followed by single curly quote',
            'disrupt’', 'bullshit’');
 
-    assert('disrupt to bullshit when starting the string and followed by single curly quote',
+    assert('disrupt => bullshit when starting the string and followed by single curly quote',
            'disrupt’', 'bullshit’');
 
-    assert('disrupt to bullshit when other punctuation follows quote',
+    assert('disrupt => bullshit when other punctuation follows quote',
            '"disrupt":', '"bullshit":');
 
-    assert('disrupt to bullshit when followed by preposition, conjunction, or verb',
+    assert('disrupt => bullshit when followed by preposition, conjunction, or verb',
            'disrupt until', 'bullshit until');
 
   });
 
   describe("phrases related to the conference", function() {
 
-    assert('Disrupt <place name> to Bullshitpalooza <place name>',
+    assert('Disrupt <place name> => Bullshitpalooza <place name>',
            'Disrupt NY', 'Bullshitpalooza NY');
 
-    assert('Disrupt Hardware to Bullshitpalooza Hardware',
+    assert('Disrupt Hardware => Bullshitpalooza Hardware',
            'Disrupt Hardware', 'Bullshitpalooza Hardware');
 
-    assert('Disrupt Battlefield to Bullshitpalooza Battlefield',
+    assert('Disrupt Battlefield => Bullshitpalooza Battlefield',
            'Disrupt Battlefield', 'Bullshitpalooza Battlefield');
 
-    assert('TechCrunch Disrupt to TechCrunch Bullshitpalooza',
+    assert('TechCrunch Disrupt => TechCrunch Bullshitpalooza',
            'going to TechCrunch Disrupt now', 'going to TechCrunch Bullshitpalooza now');
 
     assert('is not the conference name if it is lowercase',
@@ -250,25 +250,25 @@ describe("createVerbConverter", function() {
 
   describe("special phrases", function() {
 
-    assert('disruption of to bullshitting of',
+    assert('disruption of => bullshitting of',
            'disruption of', 'bullshitting of');
     
-    assert('so disruptive to such bullshit',
+    assert('so disruptive => such bullshit',
            'so disruptive', 'such bullshit');
     
-    assert('-disrupting to -bullshitting',
+    assert('-disrupting => -bullshitting',
            'industry-disrupting factors', 'industry-bullshitting factors');
     
-    assert('Disruption of to Bullshitting of',
+    assert('Disruption of => Bullshitting of',
            'Disruption of', 'Bullshitting of');
     
-    assert('So Disruptive to Such Bullshit',
+    assert('So Disruptive => Such Bullshit',
            'So Disruptive', 'Such Bullshit');
     
-    assert('so Disruptive to Such Bullshit',
+    assert('so Disruptive => Such Bullshit',
            'so Disruptive', 'Such Bullshit');
     
-    assert('-Disrupting to -Bullshitting',
+    assert('-Disrupting => -Bullshitting',
            'Industry-Disrupting Factors', 'Industry-Bullshitting Factors');
     
   });
@@ -276,16 +276,16 @@ describe("createVerbConverter", function() {
 
   describe("fallback case for things this code hasn't dealt with", function() {
 
-    assert('disrupt to bullshit when preceded by random letters',
+    assert('disrupt => bullshit when preceded by random letters',
            'randomlettersdisrupt', 'randomlettersbullshit');
 
-    assert('disrupt to bullshit when followed by random letters',
+    assert('disrupt => bullshit when followed by random letters',
            'disruptrandomletters', 'bullshitrandomletters');
 
-    assert('Disrupt to Bullshit when preceded by random letters',
+    assert('Disrupt => Bullshit when preceded by random letters',
            'randomlettersDisrupt', 'randomlettersBullshit');
 
-    assert('Disrupt to Bullshit when followed by random letters',
+    assert('Disrupt => Bullshit when followed by random letters',
            'Disruptrandomletters', 'Bullshitrandomletters');
 
   });
