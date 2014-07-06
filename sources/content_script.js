ba664f3cf8a9e78005357030aaff6ff4f0234b8a
Richard Harrington <harrington.richard@gmail.com>
2014-07-06 11:45:54 -0700

    changed description

--- sources/content_script.js
+++ sources/content_script.js
@@ -4,18 +4,15 @@
 // (and inspired by people who made Cloud to Butt.)
 
 (function(globalNamespace) {
+
   var DISRUPT_TO_BULLSHIT_RULES = globalNamespace.DISRUPT_TO_BULLSHIT_RULES;
   var walkTextNodes = globalNamespace.walkTextNodes;
   var createVerbConverter = globalNamespace.createVerbConverter;
 
-  window.addEventListener('load', makeItSo);
-
-  function makeItSo() {
-    var disruptToBullshit = createVerbConverter(DISRUPT_TO_BULLSHIT_RULES);
-    walkTextNodes(document.body, function(node) {
-      node.nodeValue = disruptToBullshit(node.nodeValue);
-    });
-  }
+  var disruptToBullshit = createVerbConverter(DISRUPT_TO_BULLSHIT_RULES);
+  walkTextNodes(document.body, function(node) {
+    node.nodeValue = disruptToBullshit(node.nodeValue);
+  });
 
 })(window);
 
