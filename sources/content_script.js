ed4127c5143a03f675f74ddfdfef29982ade2920
Richard Harrington <harrington.richard@gmail.com>
2016-03-16 17:32:09 -0400

    halfway towards getting a widget to toggle it on and off

--- sources/content_script.js
+++ sources/content_script.js
@@ -2,16 +2,34 @@
 // Use of this source code is governed by the Mozilla Public License 2.0.
 
 // (and inspired by people who made Cloud to Butt.)
-
 (function(globalNamespace) {
 
-  var DISRUPT_TO_BULLSHIT_RULES = globalNamespace.DISRUPT_TO_BULLSHIT_RULES;
-  var walkTextNodes = globalNamespace.walkTextNodes;
-  var createVerbConverter = globalNamespace.createVerbConverter;
+    var DISRUPT_TO_BULLSHIT_RULES = globalNamespace.DISRUPT_TO_BULLSHIT_RULES;
+    var walkTextNodes = globalNamespace.walkTextNodes;
+    var createVerbConverter = globalNamespace.createVerbConverter;
 
-  var disruptToBullshit = createVerbConverter(DISRUPT_TO_BULLSHIT_RULES);
-  walkTextNodes(document.body, function(node) {
-    node.nodeValue = disruptToBullshit(node.nodeValue);
-  });
-
+    var disruptToBullshit = createVerbConverter(DISRUPT_TO_BULLSHIT_RULES);
+    chrome.storage.local.get("enabled", function(storedData) {
+        if (storedData.enabled) {
+            walkTextNodes(document.body, function(node) {
+                node.nodeValue = disruptToBullshit(node.nodeValue);
+            });
+        }
+    })
 })(window);
+
+
+
+// console.log("hi from the content script");
+// //alert("hi from the content script");
+
+chrome.runtime.onMessage.addListener(
+    function(request) {
+        console.log("received message", request)
+        if (request.reload) {
+            window.location.reload();
+        }
+        if (request.debug) {
+            console.log(request.debug);
+        }
+    });
---
+++ sources/popup.js
@@ -0,0 +1,52 @@
+function toggleElementText(el, text1, text2) {
+    if (el.innerHTML == text1) {
+        el.innerHTML = text2;
+    }
+    else {
+        el.innerHTML = text1;
+    }
+}
+
+function isEnabled(callback) {
+    chrome.storage.local.get("enabled", function(storedData) {
+        callback(storedData.enabled);
+    });
+}
+
+function toggleEnabled(callback) {
+    isEnabled(function(enabled) {
+        chrome.storage.local.set({"enabled": !enabled}, callback);
+    });
+}
+
+function sendMessageToActiveTab(msg) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+    });
+}
+
+function reloadActiveTab() {
+    sendMessageToActiveTab({reload: true});
+}
+
+var submitButton = document.getElementById("submitButton");
+var reloadCheckbox = document.getElementById("reloadCheckbox");
+
+isEnabled(function(enabled) {
+    submitButton.innerHTML = enabled ? "Disable Translation" : "Enable Translation";
+});
+
+
+
+submitButton.addEventListener("click", function() {
+    toggleElementText(submitButton, "Disable Translation", "Enable Translation");
+    sendMessageToActiveTab({"debug": {"reloadCheckbox": reloadCheckbox.checked}});
+
+    toggleEnabled(function() {
+        //reloadActiveTab();
+
+        // if (reloadCheckbox.checked) {
+        //     reloadActiveTab();
+        // }
+    })
+});
